fd9ff8a67872d93ffc19e3b2efc839e1204d933d
koba saaya <saaya.koba0401@gmail.com>
2025-01-07 12:09:44 +0900

    確認と修正

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -15,6 +15,7 @@ class ProductController extends Controller
         $products=Product::Paginate(6);
         return view('index', compact('products'));
     }
+
     public function search(Request $request){
         $query=Product::with('seasons');
         if($request->has('keyword')){
@@ -28,22 +29,26 @@ class ProductController extends Controller
         $products->appends($request->query());
         return view('index', ['products'=>$products, 'keyword'=>$request->keyword, 'sort'=>$request->sort]);
     }
+
     public function detail($productId){
         $product=Product::find($productId);
         $seasons=Season::all();
         $selectedSeasons=$product->seasons->pluck('id')->toArray();
         return view('detail', compact('product', 'selectedSeasons', 'seasons'));
     }
+
     public function add(){
         $seasons=Season::all();
         return view('register', compact('seasons'));
     }
+
     public function register(ProductRequest $request){
         $productData=$request->only(['name', 'price', 'image', 'description']);
         $product=Product::create($productData);
         $product->seasons()->attach($request->seasons);
         return redirect('/products');
     }
+
     public function update(ProductRequest $request, $productId){
         $product=Product::find($productId);
         $imagePath = $request->has('image') ? $request->image : $product->image;
@@ -55,10 +60,12 @@ class ProductController extends Controller
         $product->seasons()->sync($request->seasons);
         return redirect('/products');
     }
+
     public function destroy($productId){
         $product=Product::find($productId)->delete();
         return redirect('/products');
     }
+
     public function sort(Request $request){
         $query=Product::query();
         if($request->has('search')){
